3b18740d11f19ec5ee47a856e27cf8897ddee59c
Deepan <venomskull@gmail.com>
2021-07-08 23:47:32 +1200

    after first scaffolding workable

---
+++ BloodBankWebApi/DonationBank/Migrations/20210706151920_NewMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DonationBank.Migrations
+{
+    public partial class NewMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DCandidates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Mobile = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Age = table.Column<int>(nullable: false),
+                    BloodGroup = table.Column<string>(type: "nvarchar(3)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DCandidates", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DCandidates");
+        }
+    }
+}
